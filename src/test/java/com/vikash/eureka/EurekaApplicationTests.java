6507201a08d88c02fb9279ca467b6ddb5ece99cb
vikash9110 <vikashloveindia@gmail.com>
2020-11-18 18:55:52 +0530

    first commit

---
+++ src/test/java/com/vikash/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikash.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
